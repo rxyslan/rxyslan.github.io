1b1951ce2428ae87c391386f357e0ab8567ff18b
Rayslan Vieira <43919194+rxyslan@users.noreply.github.com>
2023-02-20 15:45:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
-// Função para animar elementos da página quando eles se tornam visíveis
-function animateOnScroll() {
-    const elements = document.querySelectorAll('.animate');
-  
-    elements.forEach(element => {
-      if (element.getBoundingClientRect().top < window.innerHeight * 0.8) {
-        element.classList.add('animated');
-      }
-    });
-  }
-  
-  // Função para validar o formulário
-  function validateForm(event) {
-    event.preventDefault();
-  
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const messageInput = document.getElementById('message');
-  
-    let formIsValid = true;
-  
-    if (nameInput.value === '') {
-      nameInput.classList.add('error');
-      formIsValid = false;
-    } else {
-      nameInput.classList.remove('error');
-    }
-  
-    if (emailInput.value === '') {
-      emailInput.classList.add('error');
-      formIsValid = false;
-    } else {
-      emailInput.classList.remove('error');
-    }
-  
-    if (messageInput.value === '') {
-      messageInput.classList.add('error');
-      formIsValid = false;
-    } else {
-      messageInput.classList.remove('error');
-    }
-  
-    if (formIsValid) {
-      alert('Formulário enviado com sucesso!');
-      document.querySelector('form').reset();
-    }
-  }
-  
-  // Adiciona o evento de rolagem da página para animar elementos
-  window.addEventListener('scroll', animateOnScroll);
-  
-  // Adiciona o evento de envio para o formulário
-  document.querySelector('form').addEventListener('submit', validateForm);
-  
+// Seleciona o formulário
+const form = document.querySelector('form');
+
+// Seleciona o botão de envio
+const submitBtn = document.querySelector('#submit-btn');
+
+// Seleciona o elemento de mensagem de sucesso
+const successMsg = document.querySelector('#success-msg');
+
+// Adiciona um listener para o envio do formulário
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Exibe a mensagem de sucesso
+  successMsg.style.display = 'block';
+
+  // Esconde a mensagem de sucesso após 3 segundos
+  setTimeout(() => {
+    successMsg.style.display = 'none';
+  }, 3000);
+});
